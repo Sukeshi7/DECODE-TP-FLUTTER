5ad24f75e1a2cf3f03229696b168c18363a153ea
Thomas Coichot <coichot.t@gmail.com>
2026-06-04 11:18:58 +0200

    GL HF

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../notifiers/session_notifier.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,13 +11,15 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-
   @override
   Widget build(BuildContext context) {
     final ColorScheme colorScheme = Theme.of(context).colorScheme;
 
     return Scaffold(
       key: const ValueKey('home_screen'),
+      appBar: AppBar(
+        title: const Text('Liste des produits'),
+      ),
       backgroundColor: colorScheme.surface,
       body: const Placeholder(), // TODO: A implémenter
     );
